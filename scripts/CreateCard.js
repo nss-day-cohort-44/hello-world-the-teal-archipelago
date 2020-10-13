5c99a91a381bc3d66c4f288a3f58f67d3d4af883
billwishop <willbishop.hello@gmail.com>
2020-10-13 13:59:06 -0500

    Functions are rendering HTML.

--- scripts/CreateCard.js
+++ scripts/CreateCard.js
@@ -1,5 +1,5 @@
-const createCard=(myArray)=>{
-    return `<div class=card>
+export const createCard=(myArray)=>{
+    return `<div class="card">
         <h2>${myArray.name}</h2>
         <img src=${myArray.image} >
         <p>${myArray.description}</p>
