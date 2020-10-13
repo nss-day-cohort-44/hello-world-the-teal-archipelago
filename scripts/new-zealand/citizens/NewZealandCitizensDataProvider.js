fe493737e984707ad5a75fec0434fadecbcfe8ed
billwishop <willbishop.hello@gmail.com>
2020-10-13 11:24:53 -0500

    Added missing comma.

--- scripts/new-zealand/citizens/NewZealandCitizensDataProvider.js
+++ scripts/new-zealand/citizens/NewZealandCitizensDataProvider.js
@@ -3,14 +3,14 @@
 const citizenData = [
     {
         name: "Flight of the Conchords",
-        image: '"https://hbomax-images.warnermediacdn.com/images/GVU2fVwUV4FFvjSoJAUAV/tile.jpeg?size=1280x720&format=jpeg&partner=hbomaxcom&productCode=hbomax&host=artist.api.cdn.hbo.com&w=960"'
+        image: '"https://hbomax-images.warnermediacdn.com/images/GVU2fVwUV4FFvjSoJAUAV/tile.jpeg?size=1280x720&format=jpeg&partner=hbomaxcom&productCode=hbomax&host=artist.api.cdn.hbo.com&w=960"',
         description: "Flight of the Conchords is a New Zealand comedy duo composed of musicians Bret McKenzie and Jemaine Clement. The duo's comedy and music became the basis of a BBC radio series and then an American television series that aired for two seasons on HBO."
 
     },
     {
         name: "Sir Ernest Rutherford",
         image: '"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Ernest_Rutherford_LOC.jpg/220px-Ernest_Rutherford_LOC.jpg""https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Ernest_Rutherford_LOC.jpg/220px-Ernest_Rutherford_LOC.jpg"',
-        description: "Ernest Rutherford, 1st Baron Rutherford of Nelson, OM, FRS, HonFRSE was a New Zealand–born British physicist who came to be known as the father of nuclear physics. Encyclopædia Britannica considers him to be the greatest experimentalist since Michael Faraday."
+        description: "Ernest Rutherford, 1st Baron Rutherford of Nelson, OM, FRS, HonFRSE was a New Zealand–born British physicist who came to be known as the father of nuclear physics. Encyclopædia Britannica considers him to be the greatest experimentalist since Michael Faraday.",
     },
     {
         name: "Russell Crowe",
