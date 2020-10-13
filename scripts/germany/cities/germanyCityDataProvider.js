593904af8f1e3ceba8f71432ef73530dadaf9d23
Knevels1 <kyle.a.nevels@icloud.com>
2020-10-13 11:43:47 -0500

    fixed key name to match team members keys

--- scripts/germany/cities/germanyCityDataProvider.js
+++ scripts/germany/cities/germanyCityDataProvider.js
@@ -2,17 +2,17 @@ const cityCollection = [
     {
         name: "Berlin",
         image: "./images/Berlin.jpg",
-        text: "Berlin has the largest train station in Europe. Berlin is also nine times bigger than Paris."
+        description: "Berlin has the largest train station in Europe. Berlin is also nine times bigger than Paris."
     },
     {
         name: "Munich",
         image: "./images/Munich.jpg",
-        text: "The oldest building in Munich is a bathroom. Munich is furhter north than any major US city (excluding Alaska).",
+        description: "The oldest building in Munich is a bathroom. Munich is furhter north than any major US city (excluding Alaska).",
     },
     {
         name: "Frankfurt",
         image: "./images/frankfurt.jpg",
-        text: "Frankfurt is Germany's financial capital, along with being one of the largest airports in Europe.",
+        description: "Frankfurt is Germany's financial capital, along with being one of the largest airports in Europe.",
     },
 ]
 export const cityInfo = () => {
--- scripts/germany/landmarks/germanyLandmarksDataProvider.js
+++ scripts/germany/landmarks/germanyLandmarksDataProvider.js
@@ -2,19 +2,19 @@ const landmarkCollection = [
     {
         name: "Sanssouci Palace",
         image: "./images/Sanssouci_Palace.jpg",
-        text: "Despite Frederick the Great’s wish to be buried in Sanssouci in 1786, his wish only came true in 1991, 205 years after his death. Sanssouci Palace also hosted many prominent philosophers and artists of that period.",
+        description: "Despite Frederick the Great’s wish to be buried in Sanssouci in 1786, his wish only came true in 1991, 205 years after his death. Sanssouci Palace also hosted many prominent philosophers and artists of that period.",
 
     },
     {
         name: "Monument to the Battle of the Nations",
         image: "./images/Monument_Battle_of_the_Nations.jpg",
-        text: `The monument commemorates the defeat of Napoleon's French army at Leipzig, was paid mostly by the donations and the city of Leipzig.`,
+        description: `The monument commemorates the defeat of Napoleon's French army at Leipzig, was paid mostly by the donations and the city of Leipzig.`,
 
     },
     {
         name: "The Nuremberg Christmas Market",
         image: "./images/Nuremberg_Christmas_Market.jpg",
-        text: `The Nuremberg Christmas Market is one of the oldest Christmas Markets in Germany. It has been around since 1969.`,
+        description: `The Nuremberg Christmas Market is one of the oldest Christmas Markets in Germany. It has been around since 1969.`,
 
     },
     
