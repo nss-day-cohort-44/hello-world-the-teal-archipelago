02d36d8a5d33a5a8071f97bf68b0cf77d7135f39
Knevels1 <kyle.a.nevels@icloud.com>
2020-10-13 14:51:23 -0500

    added in the missing quotation marks to createCard.js class, and fixed landmarks id inside the html

--- scripts/CreateCard.js
+++ scripts/CreateCard.js
@@ -1,6 +1,6 @@
 
 export const createCard=(myArray)=>{
-    return `<div class=card>
+    return `<div class="card">
         <h2>${myArray.name}</h2>
         <img src=${myArray.image} >
         <p>${myArray.description}</p>
